eb71744d19c215ce7d6e5eb8214d0b55ce1741df
Trevin <trevindjones@gmail.com>
2022-07-25 22:38:03 -0600

    Some refactoring and cleanup

--- __init__.py
+++ __init__.py
@@ -12,12 +12,12 @@ except ImportError:
     pass
 
 
-def tts(p_text: str,
-        p_filename: str,
-        p_adapter: APIAdapter=None,
-        p_voice_name: str=None,
-        p_speed: str=None,
-        p_language: str=None,
+def tts(text: str,
+        filename: str,
+        specified_adapter: APIAdapter=None,
+        voice_name: str=None,
+        speed: str=None,
+        language: str=None,
         debug: bool=False) -> None:
     """
     Generate text to speech and save it to a file.
@@ -27,7 +27,7 @@ def tts(p_text: str,
         text: The text to get TTS from.
         filename: The output mp3 file. Include ".mp3"
     Optional:
-        adapter: The adapter to use, if a specific adapter is desired.
+        specified_adapter: The adapter to use, if a specific adapter is desired.
         voice_name: The name of the voice to use. Uses default if not passed.
         speed: The speed to read the text. Uses default if not passed.
         language: The language of the text. Defaults to English US.
@@ -46,50 +46,49 @@ def tts(p_text: str,
     If the adapter does not support the selected speed, it will override to the default.
     """
     adapter = None
-    adapter_instance = None
 
-    if p_adapter is not None:
-        if not issubclass(p_adapter, APIAdapter):
+    if specified_adapter is not None:
+        if not issubclass(specified_adapter, APIAdapter):
             raise TypeError("Adapter must be an instance of APIAdapter")
 
-        temp_adapter = p_adapter()
-        if p_voice_name is not None:
-            if not p_voice_name in temp_adapter:
+        temp_adapter = specified_adapter()
+        if voice_name is not None:
+            if not voice_name in temp_adapter:
                 raise ValueError("Specified voice name not found in specified adapter")
-        if p_language is not None:
-            if not p_language in temp_adapter:
+        if language is not None:
+            if not language in temp_adapter:
                 raise ValueError("Specified language not found in specified adapter")
 
-        adapter = p_adapter
+        adapter = specified_adapter
 
 
-    elif p_voice_name is not None and p_language is not None:
+    elif voice_name is not None and language is not None:
         # Loop through and see if there is a valid adapter for the language and voice
         for i_adapter in valid_adapters:
             # Instance temporary adapter so we can check if it has the voice
             temp_adapter = i_adapter()
             # If the adapter has the voice and language, use it
-            if p_voice_name in temp_adapter and \
-                    p_language in temp_adapter:
+            if voice_name in temp_adapter and \
+                    language in temp_adapter:
                 adapter = i_adapter
                 break
         # If we didn't find a valid adapter, raise an error
         if not adapter:
             raise ValueError("Language and voice combination not supported by any adapters. This could mean an optional dependency is not installed.")
     
-    elif p_voice_name is not None:
+    elif voice_name is not None:
         for i_adapter in valid_adapters:
             temp_adapter = i_adapter()
-            if p_voice_name in temp_adapter:
+            if voice_name in temp_adapter:
                 adapter = i_adapter
                 break
         if not adapter:
             raise ValueError("Voice not supported by any adapters. This could mean an optional dependency is not installed.")
 
-    elif p_language is not None:
+    elif language is not None:
         for i_adapter in valid_adapters:
             temp_adapter = i_adapter()
-            if p_language in temp_adapter:
+            if language in temp_adapter:
                 adapter = i_adapter
                 break
         if not adapter:
@@ -101,7 +100,7 @@ def tts(p_text: str,
 
 
     tts_adapter = adapter(debug)
-    tts_adapter.configure_voice(voice=p_voice_name, language=p_language, speed=p_speed)
-    tts_adapter.generate_tts(p_text)
-    tts_adapter.save_tts(p_filename)
+    tts_adapter.configure_voice(voice=voice_name, language=language, speed=speed)
+    tts_adapter.generate_tts(text)
+    tts_adapter.save_tts(filename)
     tts_adapter.finish()
