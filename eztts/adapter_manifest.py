7e179af3c87ae39df979011a18d3de6dab22d342
Trevin <tr3vinj@gmail.com>
2022-08-23 22:49:24 -0600

    converted to adapter manifest structure

---
+++ eztts/adapter_manifest.py
@@ -0,0 +1,12 @@
+ADAPTER_MANIFEST = {
+    "ftts": {
+        "import_location": ".adapters.ftts",
+        "class_name": "FTTSAdapter",
+        "remote_requirements_txt": False,
+    },
+    "gtts": {
+        "import_location": ".adapters.gtts",
+        "class_name": "GTTSAdapter",
+        "remote_requirements_txt": True,
+    },
+}
